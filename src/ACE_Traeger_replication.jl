d14984112c454abca33882bd1058f74849719c94
justinenayral <justine.nayral@sciencespo.fr>
2024-04-22 11:37:37 +0200

    Version 1

--- src/ACE_Traeger_replication.jl
+++ src/ACE_Traeger_replication.jl
@@ -1,5 +1,12 @@
 module ACE_Traeger_replication
 
+import MAT #To open .mat datasets
+import Printf #For labelling graphs
+import LinearAlgebra
+import DataFrames
+import CSV
+import XLSX
+
 greet() = print("Hello World!")
 
 end # module ACE_Traeger_replication
